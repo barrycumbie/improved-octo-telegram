d9406a478bfe13c1bb64c0d0492a98dd9cee9ccd
barry cumbie <barrycumbie@gmail.com>
2022-02-07 10:30:11 -0600

    switch which static line to use

--- server.js
+++ server.js
@@ -23,8 +23,8 @@ const app = express();
 
 console.log('serving node, expressive');
 
-app.use(express.static('./dist/mean-app'));
-// app.use(express.static(__dirname + '/dist/mean-app'));
+// app.use(express.static('./dist/mean-app'));
+app.use(express.static(__dirname + '/dist/mean-app'));
 
 
 app.get('/', function(req, res) {
